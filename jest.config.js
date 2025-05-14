d5da9a94275df8386c1ac7f3cd6e6c8136c823f2
Nasoavina <nasoavina.m@gmail.com>
2025-05-14 14:32:09 +0200

    Fix CI/CD configuration and update to ESM modules

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,21 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts'],
   coverageDirectory: 'coverage',
--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,3 +1,5 @@
+import { describe, it, expect } from '@jest/globals';
+
 describe('Utils', () => {
   it('should pass a basic test', () => {
     expect(true).toBe(true);
